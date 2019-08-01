b71b1df8859a060c45544251457c5fac014ba721
awet melake <awetmelake@gmail.com>
2019-08-01 17:40:36 -0500

    changed to functional component

--- src/components/Weeklyforecastitem.js
+++ src/components/Weeklyforecastitem.js
@@ -1,30 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Weeklyforecastitem extends Component {
-  render() {
-    const { changeFocus, f, weekly, getTime, getDay } = this.props;
-    let index = 1;
-    return weekly.list.map(item => {
-      let date = new Date(item.dt * 1000);
-      return (
-        <div
-          onClick={changeFocus.bind(this, index++)}
-          className={`weekly-forecast-item btn`}
-          key={item.dt}
-        >
-          <img
-            className="weekly-icon"
-            src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
-            alt="weather icon"
-            width="40px"
-          />
+const Weeklyforecastitem = ({ changeFocus, f, weekly, getTime, getDay }) => {
+  let index = 1;
+  return weekly.list.map(item => {
+    let date = new Date(item.dt * 1000);
+    return (
+      <div
+        onClick={changeFocus.bind(this, index++)}
+        className={`weekly-forecast-item btn`}
+        key={item.dt}
+      >
+        <img
+          className="weekly-icon"
+          src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
+          alt="weather icon"
+          width="40px"
+        />
 
-          <p>{`${date.getDate()}  ${date.getHours()}`}</p>
-          <p>{f(item.main.temp)}</p>
-        </div>
-      );
-    });
-  }
-}
+        <p>{getTime(date)}</p>
+        <p>{f(item.main.temp)}</p>
+      </div>
+    );
+  });
+};
 
 export default Weeklyforecastitem;

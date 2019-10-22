88fdc035a4418fe56f455a5504a602e4a4b60b5b
awet melake <awetmelake@gmail.com>
2019-10-22 08:19:44 -0500

    added loading init and complete action dispatches

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import weatherReducer from "./weatherReducer";
 import unitReducer from "./unitReducer";
 import locationReducer from "./locationReducer";
+import uiReducer from "./uiReducer";
 
 export default combineReducers({
   weather: weatherReducer,
   unit: unitReducer,
-  location: locationReducer
+  location: locationReducer,
+  ui : uiReducer
 });
